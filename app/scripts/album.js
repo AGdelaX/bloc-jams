61e357b0fb9b48008661971e732f94f4b6385cc6
AGdelaX <agdelax@gmail.com>
2015-05-17 10:57:52 -0400

    Added functionality to seek bar

--- app/scripts/album.js
+++ app/scripts/album.js
@@ -1,4 +1,11 @@
 //Example Album
+
+//Bonus: a function that prints
+//the X and Y coordinates of the click action
+$(document).click(function(){
+  console.log(event.pageX, event.pageY);
+});
+
 var albumPicasso = {
 	name: 'The Colors',
 	artist: 'Pablo Picasso',
@@ -128,6 +135,44 @@ var changeAlbumView = function(album) {
  
  };
 
+ var updateSeekPercentage = function($seekBar, event) {
+  var barWidth = $seekBar.width();
+  var offsetX = event.pageX - $seekBar.offset().left; // get mouse x offset here
+
+  var offSetXPercent = (offsetX / barWidth) * 100;
+  offSetXPercent = Math.max(0, offSetXPercent);
+  offSetXPercent = Math.min(100, offSetXPercent);
+
+  var percentageString = offSetXPercent + '%';
+  $seekBar.find('.fill').width(percentageString);
+  $seekBar.find('.thumb').css({left: percentageString});
+ }
+
+ var setupSeekBars = function(){
+  $seekBars = $('.player-bar .seek-bar');
+  $seekBars.click(function(event){
+    updateSeekPercentage($(this), event);
+  });
+
+  $seekBars.find('.thumb').mousedown(function(event){
+    var $seekBar = $(this).parent();
+
+    $seekBar.addClass('no-animate');
+
+    $(document).bind('mousemove.thumb', function(event){
+      updateSeekPercentage($seekBar, event);
+    });
+
+    $(document).bind('mouseup.thumb', function(){
+
+      $seekBar.removeClass('no-animate');
+
+      $(document).unbind('mousemove.thumb');
+      $(document).unbind('mouseup.thumb');
+    });
+  });
+ };
+
 
 // This 'if' condition is used to prevent the jQuery modifications
  // from happening on non-Album view pages.
@@ -137,7 +182,7 @@ var changeAlbumView = function(album) {
    $(document).ready(function() {
     
     changeAlbumView(albumPicasso);
-
+      setupSeekBars();
 
     		$('.click-next-album img').click(function(){
 			changeAlbumView(albumMarconi);
