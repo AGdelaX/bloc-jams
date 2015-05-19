136cbcd2db076b0e546440843256459ef2161c91
AGdelaX <agdelax@gmail.com>
2015-05-18 21:36:46 -0400

    Converted collection view to angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -294,6 +294,22 @@ var changeAlbumView = function(album) {
 // require('./album');
 // require('./profile');
 
+var albumPicasso = {
+   name: 'The Colors',
+   artist: 'Pablo Picasso',
+   label: 'Cubism',
+   year: '1881',
+   albumArtUrl: '/images/album-placeholder.png',
+ 
+   songs: [
+       { name: 'Blue', length: '4:26' },
+       { name: 'Green', length: '3:14' },
+       { name: 'Red', length: '5:01' },
+       { name: 'Pink', length: '3:21'},
+       { name: 'Magenta', length: '2:15'}
+     ]
+ };
+
 blocJams = angular.module('BlocJams', ['ui.router']);
 
 blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
@@ -310,6 +326,12 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
      controller: 'Song.controller',
      templateUrl: '/templates/song.html'
    	});
+
+   $stateProvider.state('collection', {
+   		url: '/collection',
+   		controller: 'Collection.controller',
+   		templateUrl: '/templates/collection.html'
+   });
 }]);
 
 blocJams.run(function () {
@@ -347,6 +369,13 @@ blocJams.controller('Landing.controller', ['$scope', function($scope) {
 
 blocJams.controller('Song.controller', ['$scope', function($scope) {
 	alert('Song controller loaded!')
+}]);
+
+blocJams.controller('Collection.controller', ['$scope', function($scope) {
+	$scope.albums = [];
+		for (var i = 0; i < 33; i++) {
+			$scope.albums.push(angular.copy(albumPicasso));
+		}
 }]);
 });
 
